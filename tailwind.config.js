d3a921f2b651d78389b2a889fde5c7b09a03aa52
maksymrybalchenkogh <rybalchenko.mx@gmail.com>
2021-04-11 13:21:53 +0300

    tailwindcss added

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// https://jacobneterer.medium.com/angular-and-tailwindcss-2388fb6e0bab
+require('dotenv').config();
+const enablePurge = process.env.ENABLE_PURGE || false;
+module.exports = {
+  purge: {
+    enabled: enablePurge,
+    content: [
+      './src/**/*.html',
+      './src/**/*.scss'
+    ]
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
